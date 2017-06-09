119d905e0f3ccdea6c78a43efa709372ceb5534e
Rohan <rohan.mishra1997@gmail.com>
2017-06-10 00:39:59 +0530

    Init

---
+++ config/secret.js
@@ -0,0 +1,6 @@
+module.exports = {
+	'page_access_token' : 'EAAamZAGbezIgBAIQZCG2SS7nSGjbm51RlOGfpOSQEa99G5HG9cipUBdPz0JO90CfsQ0fWMT1rpa6CBDLqSy4nZBB9z4NYJljVCAkmLZCWeDathOifnZAydDgduZBswJ1JW6ZAb1b4itvSZB4EPbhIVkkSy32l1oC1sOna2jHyvhZByQZDZD',
+	'app_id':'1871800013081736',
+	'app_secret':'95c32d826609e5c821bc71a90f62b259',
+	'verify_token':'rohantest'
+}
